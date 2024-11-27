8aeb56e31864e1114b545d0a41c5e6feb46642fd
hefraimjr <zfollen888@gmail.com>
2024-11-27 11:15:40 -0400

    continuação da aula 8
    hehehe

--- cliente/src/pages/Registro.jsx
+++ cliente/src/pages/Registro.jsx
@@ -1,8 +1,24 @@
 import { useState } from "react";
 
 export default function Registrar() {
-event.preventDefault();
+const [nome, setNome] = useState ("");
+const[email, setEmail] = useState("")
 
+const resgistrar = async (event) => {
+  event.preventeDaefault();
+  try{
+    await fetch('http://locoalhost:3000/usuarios',{
+      method: 'POST' , 
+      headers: { 'Content-Type':'Application/json'},
+      body: JSON.stringify({
+        nome:nome,
+        email:email
+      })
+    })
+  }catch{
+    
+  }
+}
   return (
         
   );
